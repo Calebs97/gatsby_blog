8d2eaf4572e7fa0d2df3082c2946207943b753d5
Caleb Siu <calebs11@berkeley.edu>
2018-10-23 15:09:52 -0700

    changes sass file

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,7 +5,7 @@ class Sidebar extends Component {
     return (
       <header id="header">
           <div class="inner">
-            <a href="/" class="image avatar"><img src="images/avatar.jpg" alt="" /></a>
+            <a href="#" class="image avatar"><img src="images/avatar.jpg" alt="" /></a>
             <h1>Hello world! <strong>I'm Caleb</strong>, <br />
               welcome to my website!<br /></h1>
           </div>
