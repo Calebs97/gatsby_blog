e7b1a0aedad49479ea5210c30129e1d09556bfad
Caleb Siu <calebs11@berkeley.edu>
2018-10-24 00:52:10 -0700

    Updates

---
+++ precache-manifest.002fcfabe47d3776675769a563d35d5a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "900537873d7093348ed9",
+    "url": "/static/css/main.a7b31025.chunk.css"
+  },
+  {
+    "revision": "a0b93aab81d32c64341c",
+    "url": "/static/js/1.a0b93aab.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "63337cc73a290619340053d734f3b53e",
+    "url": "/static/media/overlay.63337cc7.png"
+  },
+  {
+    "revision": "eb51da5ca333e34f6564f43ab80c356e",
+    "url": "/static/media/bg.eb51da5c.jpg"
+  },
+  {
+    "revision": "900537873d7093348ed9",
+    "url": "/static/js/main.90053787.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "3853c1f95adb566ee549c7e977371230",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.614504181e8a076e62a764c0dabb5686.js"
+  "/precache-manifest.002fcfabe47d3776675769a563d35d5a.js"
 );
 
 workbox.clientsClaim();
