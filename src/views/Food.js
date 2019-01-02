218adef2d2afe240cfe8b8d3901dc8a19fd5bdd5
Caleb Siu <calebs11@berkeley.edu>
2019-01-02 13:18:19 -0500

    added initial signup functionality

--- src/views/Food.js
+++ src/views/Food.js
@@ -3,21 +3,45 @@ import Main from "../containers/Main"
 import Helmet from 'react-helmet';
 
 export default class Food extends Component { 
-  state = { 
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const json_data = {'first-name': data.get('first-name'),
+                       'last-name': data.get('last-name'),
+                       'phone-num': data.get('phone-num')}
+
+    fetch('https://mighty-badlands-71550.herokuapp.com/signup', {
+      mode: 'cors',
+      headers: {
+        'Access-Control-Allow-Origin':'*',
+      },
+      method: 'POST',
+      body: JSON.stringify(json_data),
+    });
   }
   
   render () {                                   
       return (
         <Main>
           <Helmet title="Food" />
-            <section id="one">
-              <header class="major">
-                <h2>Food </h2>
-              </header>
-              <p>Unfortunately I am quite bad at taking pictures, hence this page will need some major
-                brainstorming as to what I should put here. More to come!!
-              </p>
-            </section>
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="username">First Name</label>
+            <input id="first-name" name="first-name" type="text" />
+
+            <label htmlFor="email">Last Name</label>
+            <input id="last-name" name="last-name" type="text" />
+
+            <label htmlFor="birthdate">Phone Number</label>
+            <input id="phone-num" name="phone-num" type="text" />
+
+            <button>Send data!</button>
+          </form>
         </Main>
       )
    }
