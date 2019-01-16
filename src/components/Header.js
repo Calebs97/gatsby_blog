58f009286c8f526cd276f83363c318bdcb7678ab
Caleb Siu <calebs11@berkeley.edu>
2019-01-16 12:00:33 -0500

    removed books, fixed bullet points

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,6 @@ class Header extends Component {
           <li><Link to='/about'>About </Link></li>
           <li><Link to='/work'>Things I've Done </Link></li>
           <li><Link to='/food'>Food </Link></li>
-          <li><Link to='/books'>Books </Link></li>
           <li><Link to='/contact'>Contact</Link></li>
           <li><Link to='/site'>This Site</Link></li>
         </ul>
